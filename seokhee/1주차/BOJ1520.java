66371f1f3a0d78779b0c7c3ef6d074a2a8a9ad56
김석희 <cat1181123@naver.com>
2024-08-29 13:25:50 +0900

    내리막길 - dfs, dp

--- seokhee/1주차/BOJ1520.java
+++ seokhee/1주차/BOJ1520.java
@@ -5,11 +5,10 @@ import java.util.StringTokenizer;
 
 public class BOJ1520 {
     static int arr[][];
-    static boolean visited[][];
     static int dx[] = { 0, 0, 1, -1 };
     static int dy[] = { 1, -1, 0, 0 };
     static int n, m;
-    static int res;
+    // dp자체로 방문처리 체크
     static int dp[][];
 
     public static void main(String[] args) throws Exception {
@@ -20,32 +19,38 @@ public class BOJ1520 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n][m];
-        visited = new boolean[n][m];
         dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
+            }
+        }
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
-        } // 입력
+        } // 입력 끝
 
-        res = 0;
-        dfs(0, 0);
-
-        System.out.println(res);
+        System.out.println(dfs(0, 0));
 
     }
 
-    static void dfs(int x, int y) {
+    static int dfs(int x, int y) {
         if (x == n - 1 && y == m - 1) {
-            res++;
-            return;
+            return 1;
         }
 
-        visited[x][y] = true;
         int cur = arr[x][y];
 
+        // 이미 방문한 곳이면 그 값 리턴
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 0;
+        // 방문하지 않은 곳
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
@@ -53,15 +58,11 @@ public class BOJ1520 {
             if (nx < 0 || ny < 0 || nx >= n || ny >= m)
                 continue;
 
-            if (visited[nx][ny])
-                continue;
-
             if (arr[nx][ny] < cur) {
-                dfs(nx, ny);
+                dp[x][y] += dfs(nx, ny);
             }
         }
 
-        visited[x][y] = false;
-
+        return dp[x][y];
     }
 }
