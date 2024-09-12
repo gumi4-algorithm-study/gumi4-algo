99eff669285e56eaf38e5046e452b3efd83e225d
김석희 <cat1181123@naver.com>
2024-09-12 17:25:13 +0900

    stack

--- 3주차/BOJ2493.java
+++ 3주차/BOJ2493.java
@@ -1,12 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class BOJ2493 {
     static int n;
-    static int[][] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
@@ -15,23 +11,37 @@ public class BOJ2493 {
         StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
-        arr = new int[n][2];
-        // Stack<int[]> stack = new Stack<>();
+
+        Stack<int[]> stack = new Stack<>();
 
         st = new StringTokenizer(br.readLine(), " ");
+
         for (int i = 0; i < n; i++) {
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = i;
+            int value = Integer.parseInt(st.nextToken());
+            if (i == 0) {
+                stack.push(new int[] { 0, value });
+                sb.append(0).append(" ");
+            } else {
+                int p = stack.peek()[1];
+                if (p >= value) {
+                    sb.append(stack.peek()[0] + 1).append(" ");
+                    stack.push(new int[] { i, value });
+                }
+                // 작아? 빼
+                else {
+                    while (!stack.isEmpty() && (stack.peek()[1] < value)) {
+                        stack.pop();
+                    }
+                    if (stack.isEmpty()) {
+                        sb.append(0).append(" ");
+                    } else {
+                        sb.append(stack.peek()[0] + 1).append(" ");
+                    }
+                    stack.push(new int[] { i, value });
+                }
+            }
         }
 
-        for (int i = 1; i < n; i++) {
-            arr[i][1] = dfs(arr[i][0], i - 1);
-        }
-
-        sb.append(0).append(" ");
-        for (int i = 1; i < n; i++) {
-            sb.append(arr[i][1] + 1).append(" ");
-        }
         System.out.println(sb);
 
         /**
@@ -51,31 +61,4 @@ public class BOJ2493 {
 
     }
 
-    // 자기보다 큰 수가 없는 거랑 => -1
-    // 0번째 idx인거랑 구분해야함 => 0
-    static int dfs(int origin, int idx) {
-
-        // 나보다 커? return
-        if (arr[idx][0] >= origin) {
-            return idx;
-        }
-
-        // 첫번째꺼가지 작으면
-        if (arr[idx][0] < origin && idx == 0)
-            return -1;
-
-        int k = 0;
-        // 나보다 작아? 더 들어가
-        if (arr[idx][0] < origin && arr[idx][1] != 0) {
-            k = dfs(origin, arr[idx][1]);
-        }
-
-        if (arr[idx][1] == 0) {
-            return 0;
-        }
-
-        return k;
-    }
 }
-// 78 1 2 3 1 1 1
-// 0 1 1 1 4 5 6
